87a75faae9aab1f0b87a4cf1b0ce077571a5f5af
dotprofile <58643089+dotprofile@users.noreply.github.com>
2025-07-11 20:45:37 -0700

    Add comments in  6717_hvFixKeys.asm
    Just adding comments

--- 6717_hvFixKeys.asm
+++ 6717_hvFixKeys.asm
@@ -1,12 +1,11 @@
-# Decompiled patch output
 .include "macros.S"
 
-MAKEPATCH 0x00003378
+MAKEPATCH 0x00003378 #   disable copying transform key from SoC
 0:
 	nop
 9:
 
-MAKEPATCH  0x00000018
+MAKEPATCH  0x00000018 # pbkey xex2key make transform key 81300D49C232F145
 0:
 	.long 0x81300D49
 	.long 0xC232F145
