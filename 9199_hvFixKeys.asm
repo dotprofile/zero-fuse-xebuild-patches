8aa42a71b2235e1b255335e95b40e0f4a87dc173
dotprofile <58643089+dotprofile@users.noreply.github.com>
2025-07-11 20:44:09 -0700

    Add comments in 9199_hvFixKeys.asm
    Just adding some comments in here

--- 9199_hvFixKeys.asm
+++ 9199_hvFixKeys.asm
@@ -3,12 +3,12 @@
 
 #HvFixKeys
 
-MAKEPATCH 0x00002A04
+MAKEPATCH 0x00002A04 #   disable copying transform key from SoC
 0:
 	nop
 9:
 
-MAKEPATCH 0x00000018
+MAKEPATCH 0x00000018 # pbkey xex2key make transform key 81300D49C232F145
 0:
     .long 0x81300D49
     .long 0xC232F145
